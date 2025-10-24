e6f7c987255324fe026348367037629870793fe0
JuniorCarti <ridgejunior204@gmail.com>
2025-10-24 22:31:30 +0300

    Add state and data loading logic to Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,3 +4,12 @@ import { CategoryCard } from '../components/CategoryCard';
 import { DataService } from '../services/dataService';
 import { ProgressService } from '../services/progressService';
 import { Module, CategoryStats, ModuleTypeStats } from '../types';
+
+export const Home: React.FC = () => {
+  const [modules, setModules] = useState<Module[]>([]);
+  const [progress, setProgress] = useState<{ [key: number]: boolean }>({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadData();
+  }, []);
