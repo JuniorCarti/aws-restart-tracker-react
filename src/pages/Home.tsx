8c83ed78d37863c425eff9f0b582ff4c5eafd62c
JuniorCarti <ridgejunior204@gmail.com>
2025-10-24 22:30:56 +0300

    Create Home page with imports for ProgressRing, CategoryCard, DataService, and ProgressService

---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { ProgressRing } from '../components/ProgressRing';
+import { CategoryCard } from '../components/CategoryCard';
+import { DataService } from '../services/dataService';
+import { ProgressService } from '../services/progressService';
+import { Module, CategoryStats, ModuleTypeStats } from '../types';
